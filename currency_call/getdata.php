794e633765920ea22a4d0dadb8f4c24876641b5b
mehrabkb <mkordbacheh75@gmail.com>
2025-11-21 09:40:55 +0330

    fix getdata.php bugs in cors error

--- currency_call/getdata.php
+++ currency_call/getdata.php
@@ -1,4 +1,15 @@
 <?php
+//ازه دسترسی از همه دامنه‌ها
+header("Access-Control-Allow-Origin: *");
+
+// اگر نیاز به متدهای خاص دارید
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+
+// اگر نیاز به هدرهای خاص دارید
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
+// ادامه کد شما...
+
     $time = json_decode(file_get_contents("lastUpdate.json"));
     $lastUpdate = $time->last_update;
     if(time() >= ($lastUpdate + 60)){
